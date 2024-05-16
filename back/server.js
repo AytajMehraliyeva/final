636f5ecb3d6812dfc535615fce6d082ae341529c
AytajMehraliyeva <tu7lzxxdc@code.edu.az>
2024-05-16 04:08:51 +0400

    .

--- back/server.js
+++ back/server.js
@@ -4,7 +4,6 @@ require('dotenv').config()
 
 const mongoose=require('mongoose')
 const RouterItem=require('./router/Card.router')
-const routerUser=require('./router/User.router')
 const app=express()
 const PORT=process.env.PORT || 3020
 
@@ -12,7 +11,6 @@ const PORT=process.env.PORT || 3020
 app.use(cors())
 app.use(express.json())
 app.use('/cards', RouterItem)
-app.use("/user", routerUser)
 
 mongoose.connect("mongodb+srv://mehraliyevaaytac15:mehraliyeva2004@cluster0.a9uh1fu.mongodb.net").then(res=>{
     console.log('Connect to db')
